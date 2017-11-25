39147409e3c3e3b69c24ca17455d62ffb38b4b78
jarretterrio <jarretterrio@live.com>
2017-11-25 15:47:59 -0400

    Adding temporary virtual Database

--- SnackTrack/SnackTrack/Models/SimulatedDatabase.swift
+++ SnackTrack/SnackTrack/Models/SimulatedDatabase.swift
@@ -9,6 +9,6 @@
 import Foundation
 
 class SimulatedDatabase: NSObject {
-    static var items = []
+    static var foodDatabase : [String : Int] = ["banana" : 89, "pizza" : 266, "bananas": 267, "apple" : 52, "sandwich": 233, "packet" : 160, "can of soda" : 41, "bar" : 110, "pasta" : 131, "burger" : 295, "hotdog" : 290, "chicken wings" : 81, "salade" : 152, "tomato" : 18 , "broccoli" : 34, "onion" : 40, "donair" : 424, "strawberry" : 33, "strawberries" : 100, "soup" : 70 ]
     
 }
