3f6313fa86f7986a511487309e9c7432147f3c0d
Mackenzie Boudreau <mack.boudreau2@gmail.com>
2017-11-25 12:56:12 -0400

    100 commits later... fixed all conflicts and brought back the project

---
+++ SnackTrack/SnackTrack/Support/Extensions/Extensions.swift
@@ -0,0 +1,53 @@
+//
+//  Extensions.swift
+//  DalHackathon2017
+//
+//  Created by Mack Boudreau on 2017-11-25.
+//  Copyright © 2017 Mack Boudreau. All rights reserved.
+//
+// Used in previous projects...
+import UIKit
+import ImageIO
+
+extension UIColor {
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor{
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+    }
+}
+
+extension UIView {
+    func addConstraintsWithFormat(format: String, views: UIView...) {
+        var viewsDictionary = [String: UIView]()
+        for (index, view) in views.enumerated() {
+            let key = "v\(index)"
+            view.translatesAutoresizingMaskIntoConstraints = false
+            viewsDictionary[key] = view
+        }
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: format, options: NSLayoutFormatOptions(), metrics: nil, views: viewsDictionary))
+        
+    }
+}
+
+extension CGImagePropertyOrientation {
+    /**
+     Converts a `UIImageOrientation` to a corresponding
+     `CGImagePropertyOrientation`. The cases for each
+     orientation are represented by different raw values.
+     
+     - Tag: ConvertOrientation
+     */
+    init(_ orientation: UIImageOrientation) {
+        switch orientation {
+        case .up: self = .up
+        case .upMirrored: self = .upMirrored
+        case .down: self = .down
+        case .downMirrored: self = .downMirrored
+        case .left: self = .left
+        case .leftMirrored: self = .leftMirrored
+        case .right: self = .right
+        case .rightMirrored: self = .rightMirrored
+        }
+    }
+}
+
